b5d913aa558e4a7d38b75e443d3ecd5fc2e46ab9
Matt Manning <matt.manning@gmail.com>
2013-07-14 23:27:21 -0400

    Client-side JS upload.

--- test/pallit.js
+++ test/pallit.js
@@ -1,39 +1,52 @@
-$(document).ready(function {
-  $(':file').change(function(){
-      var file = this.files[0];
-      name = file.name;
-      size = file.size;
-      type = file.type;
-  });
-});
+$(document).ready(function() {
 
-$(':button').click(function(){
-    var formData = new FormData($('form')[0]);
-    $.ajax({
-        url: 'http://localhost:5000/file',  //server script to process data
-        type: 'POST',
-        xhr: function() {  // custom xhr
-            var myXhr = $.ajaxSettings.xhr();
-            if(myXhr.upload){ // check if upload property exists
-                myXhr.upload.addEventListener('progress',progressHandlingFunction, false); // for handling the progress of the upload
-            }
-            return myXhr;
-        },
-        //Ajax events
-        // beforeSend: beforeSendHandler,
-        // success: completeHandler,
-        // error: errorHandler,
-        // Form data
-        data: formData,
-        //Options to tell JQuery not to process data or worry about content-type
-        cache: false,
-        contentType: false,
-        processData: false
-    });
-});
-
-function progressHandlingFunction(e){
-    if(e.lengthComputable){
-        $('progress').attr({value:e.loaded,max:e.total});
+    if (!window.FileReader) {
+      alert('The File API is not fully supported in this browser.');
+      return;
     }
-}
+
+    var pallitForm = new FormData();
+
+    // $('form.pallit input:file').change(function(){
+    //     var file = this.files[0];
+    //     pallitForm.append('file-size', file.size);
+    //     pallitForm.append('file', file);
+    // });
+
+    $('form.pallit input:submit').click(function(){
+        alert("Clicked");
+        var file = $('form.pallit input:file')[0].files[0];
+        pallitForm.append('file-size', file.size);
+        pallitForm.append('file', file);
+        
+        $.ajax({
+            url: 'http://localhost:5000/file',  //server script to process data
+            type: 'POST',
+            // xhr: function() {  // custom xhr
+            //     var myXhr = $.ajaxSettings.xhr();
+            //     if(myXhr.upload){ // check if upload property exists
+            //         myXhr.upload.addEventListener('progress',progressHandlingFunction, false); // for handling the progress of the upload
+            //     }
+            //     return myXhr;
+            // },
+            //Ajax events
+            beforeSend: function() {alert("Before send.");},
+            success: function() {alert("Uploaded!");},
+            error: function() {alert("Error!");},
+            // Form data
+            data: pallitForm,
+            //Options to tell JQuery not to process data or worry about content-type
+            cache: false,
+            contentType: false,
+            processData: false
+        });
+
+        alert("Completed");
+    });
+
+    function progressHandlingFunction(e){
+        if(e.lengthComputable){
+            $('progress').attr({value:e.loaded,max:e.total});
+        }
+    }
+});
